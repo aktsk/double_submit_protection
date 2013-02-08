5a09974695f1d11a016fff6f41e80c024ab89fb4
ssd-aktsk <n.shishido@aktsk.jp>
2013-02-08 17:14:31 +0900

    add #double_submit_token_hash to get only token_name and token_value

--- lib/double_submit_protection.rb
+++ lib/double_submit_protection.rb
@@ -5,20 +5,44 @@ module DoubleSubmitProtection
   DEFAULT_TOKEN_NAME = 'submit_token'
 
   module View
-    def double_submit_token(token_name=nil)
-      token_name ||= DEFAULT_TOKEN_NAME
-      flash[token_name] = Digest::MD5.hexdigest(rand.to_s)
-      hidden_field_tag(token_name, flash[token_name])
+    def double_submit_token_hash(id, token_name=nil)
+      key = DoubleSubmitProtection.token_key(id, token_name)
+      @double_submit_token_hash ||= {}
+      @double_submit_token_hash[key] ||=
+        begin
+          key = DoubleSubmitProtection.token_key(id, token_name)
+          value = token_value()
+          flash[key] = value
+          value
+        end
+      { key => @double_submit_token_hash[key] }
+    end
+
+    def double_submit_token(id, token_name=nil)
+      h = double_submit_token_hash(id, token_name)
+      hidden_field_tag(h.keys.first, h.values.first)
+    end
+
+    private
+
+    def token_value
+      Digest::MD5.hexdigest(rand.to_s)
     end
   end
 
   module Controller
-    def double_submit?(token_name=nil)
-      token_name ||= DEFAULT_TOKEN_NAME
-      token = flash[token_name]
-      token.nil? || ( (request.post? || request.put?) && (token != params[token_name]) )
+    def double_submit?(id, token_name=nil)
+      key = DoubleSubmitProtection.token_key(id, token_name)
+      token = flash[key]
+      token.nil? || (token != params[key])
     end
   end
+
+  def self.token_key(id, name=nil)
+    name &&= "_#{name}"
+    "#{DEFAULT_TOKEN_NAME}#{name}:#{id}"
+  end
+
 end
 
 ActiveSupport.on_load(:action_controller) do
